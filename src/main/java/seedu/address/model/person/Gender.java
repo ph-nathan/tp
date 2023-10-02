3e62238bb2d3af403d7135a4ad8d86f38c37ea5c
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-10-02 16:43:00 +0800

    Add Gender class

---
+++ src/main/java/seedu/address/model/person/Gender.java
@@ -0,0 +1,62 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's gender in CCACommander.
+ * Guarantees: immutable; is valid as declared in {@link #isValidGender(String)}
+ */
+public class Gender {
+
+    public static final String MESSAGE_CONSTRAINTS = "Gender should only be one of the following: \n"
+            + "Male, Female, or Others";
+
+    public final String value;
+
+    /**
+     * Constructs an {@code Gender}.
+     *
+     * @param gender A valid email address.
+     */
+    public Gender(String gender) {
+        requireNonNull(gender);
+        checkArgument(isValidGender(gender), MESSAGE_CONSTRAINTS);
+        value = gender;
+    }
+
+    /**
+     * Returns if a given string is a valid email.
+     */
+    public static boolean isValidGender(String test) {
+        return test.equals("Male") ||
+                test.equals("Female") ||
+                test.equals("Others");
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Gender)) {
+            return false;
+        }
+
+        Gender otherGender = (Gender) other;
+        return value.equals(otherGender.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
