a48512b35649bc7ae1871890f63c695b594c9457
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-10-06 10:19:05 +0800

    Create Skeleton Event class

---
+++ src/main/java/seedu/address/model/event/Event.java
@@ -0,0 +1,93 @@
+package seedu.address.model.event;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Objects;
+
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Represents a Person in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Event {
+
+    // Identity fields
+    private final EventName eventName;
+
+    // Data fields
+    private final Location location;
+    private final Date date;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Event(EventName eventName, Location location, Date date) {
+        requireAllNonNull(eventName, location, date);
+        this.eventName = eventName;
+        this.location = location;
+        this.date = date;
+    }
+
+    public EventName getName() {
+        return eventName;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+    public Date getDate() {
+        return date;
+    }
+}
+
+    /**
+     * Returns true if both persons have the same name.
+     * This defines a weaker notion of equality between two persons.
+     */
+    public boolean isSameEvent(Event otherEvent) {
+        if (otherEvent == this) {
+            return true;
+        }
+
+        return otherEvent != null
+                && otherEvent.getName().equals(getName());
+    }
+
+    /**
+     * Returns true if both events have the same identity and data fields.
+     * This defines a stronger notion of equality between two events.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Event)) {
+            return false;
+        }
+
+        Event otherEvent = (Event) other;
+        return eventName.equals(otherEvent.eventName)
+                && location.equals(otherEvent.location)
+                && date.equals(otherEvent.date);
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(eventName, location, date);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("name", eventName)
+                .add("location", location)
+                .add("date", date)
+                .toString();
+    }
+
+}
