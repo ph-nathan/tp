f5893dbaaeaa8a2f8a7ed93c8068753e705460af
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-10-11 18:05:03 +0800

    Add tests for attributes for events

---
+++ src/test/java/seedu/address/model/event/LocationTest.java
@@ -0,0 +1,58 @@
+package seedu.location.model.event;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.event.Location;
+
+public class LocationTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Location(null));
+    }
+
+    @Test
+    public void constructor_invalidLocation_throwsIllegalArgumentException() {
+        String invalidLocation = "";
+        assertThrows(IllegalArgumentException.class, () -> new Location(invalidLocation));
+    }
+
+    @Test
+    public void isValidLocation() {
+        // null location
+        assertThrows(NullPointerException.class, () -> Location.isValidLocation(null));
+
+        // invalid locationes
+        assertFalse(Location.isValidLocation("")); // empty string
+        assertFalse(Location.isValidLocation(" ")); // spaces only
+
+        // valid locationes
+        assertTrue(Location.isValidLocation("Blk 456, Den Road, #01-355"));
+        assertTrue(Location.isValidLocation("-")); // one character
+        assertTrue(Location.isValidLocation("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long location
+    }
+
+    @Test
+    public void equals() {
+        Location location = new Location("Valid Location");
+
+        // same values -> returns true
+        assertTrue(location.equals(new Location("Valid Location")));
+
+        // same object -> returns true
+        assertTrue(location.equals(location));
+
+        // null -> returns false
+        assertFalse(location.equals(null));
+
+        // different types -> returns false
+        assertFalse(location.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(location.equals(new Location("Other Valid Location")));
+    }
+}
