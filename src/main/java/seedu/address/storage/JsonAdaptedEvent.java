e6e4f60ef39769d80b77f7a89b07436e71224c9b
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-10-11 10:37:54 -0400

    Add JSONAdaptedEvent

---
+++ src/main/java/seedu/address/storage/JsonAdaptedEvent.java
@@ -0,0 +1,83 @@
+package seedu.address.storage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.event.Event;
+import seedu.address.model.event.EventDate;
+import seedu.address.model.event.EventName;
+import seedu.address.model.event.Location;
+
+/**
+ * Jackson-friendly version of {@link Event}.
+ */
+class JsonAdaptedEvent {
+
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Event's %s field is missing!";
+
+    private final String eventName;
+    private final String eventDate;
+    private final String location;
+    private final List<JsonAdaptedTag> tags = new ArrayList<>();
+
+    /**
+     * Constructs a {@code JsonAdaptedPerson} with the given person details.
+     */
+    @JsonCreator
+    public JsonAdaptedEvent(@JsonProperty("eventName") String name, @JsonProperty("eventDate") String eventDate,
+                             @JsonProperty("location") String location) {
+        this.eventName = name;
+        this.eventDate = eventDate;
+        this.location = location;
+    }
+
+    /**
+     * Converts a given {@code Event} into this class for Jackson use.
+     */
+    public JsonAdaptedEvent(Event source) {
+        eventName = source.getName().eventName;
+        eventDate = source.getDate().date.toString();
+        location = source.getLocation().value;
+    }
+
+    /**
+     * Converts this Jackson-friendly adapted person object into the model's {@code Person} object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted person.
+     */
+    public Event toModelType() throws IllegalValueException {
+        if (eventName == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    EventName.class.getSimpleName()));
+        }
+        if (!EventName.isValidName(eventName)) {
+            throw new IllegalValueException(EventName.MESSAGE_CONSTRAINTS);
+        }
+        final EventName modelName = new EventName(eventName);
+
+        if (eventDate == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    EventDate.class.getSimpleName()));
+        }
+        if (!EventDate.isValidDate(eventDate)) {
+            throw new IllegalValueException(EventDate.MESSAGE_CONSTRAINTS);
+        }
+        final EventDate modelEventDate = new EventDate(eventDate);
+
+        if (location == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    Location.class.getSimpleName()));
+        }
+        if (!Location.isValidLocation(location)) {
+            throw new IllegalValueException(Location.MESSAGE_CONSTRAINTS);
+        }
+        final Location modelLocation = new Location(location);
+
+        return new Event(modelName, modelEventDate, modelLocation);
+    }
+
+}
---
+++ src/test/java/seedu/address/storage/JsonAdaptedEventTest.java
@@ -0,0 +1,73 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.storage.JsonAdaptedEvent.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalEvents.AURORA_BOREALIS;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.event.EventDate;
+import seedu.address.model.event.EventName;
+import seedu.address.model.event.Location;
+
+public class JsonAdaptedEventTest {
+    private static final String INVALID_NAME = "µ@18Tur";
+    private static final String INVALID_DATE = "20/12/12";
+    private static final String INVALID_LOCATION = " ";
+
+    private static final String VALID_NAME = AURORA_BOREALIS.getName().toString();
+    private static final String VALID_DATE = AURORA_BOREALIS.getDate().toString();
+    private static final String VALID_LOCATION = AURORA_BOREALIS.getLocation().value;
+    @Test
+    public void toModelType_validEventDetails_returnsEvent() throws Exception {
+        JsonAdaptedEvent event = new JsonAdaptedEvent(AURORA_BOREALIS);
+        assertEquals(AURORA_BOREALIS, event.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidName_throwsIllegalValueException() {
+        JsonAdaptedEvent event =
+                new JsonAdaptedEvent(INVALID_NAME, VALID_DATE, VALID_LOCATION);
+        String expectedMessage = EventName.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, event::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullName_throwsIllegalValueException() {
+        JsonAdaptedEvent event = new JsonAdaptedEvent(null, VALID_DATE, VALID_LOCATION);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, EventName.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, event::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidDate_throwsIllegalValueException() {
+        JsonAdaptedEvent event = new JsonAdaptedEvent(VALID_NAME, INVALID_DATE, VALID_LOCATION);
+        String expectedMessage = EventDate.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, event::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullDate_throwsIllegalValueException() {
+        JsonAdaptedEvent event = new JsonAdaptedEvent(VALID_NAME, null, VALID_LOCATION);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, EventDate.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, event::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidLocation_throwsIllegalValueException() {
+        JsonAdaptedEvent event = new JsonAdaptedEvent(VALID_NAME, VALID_DATE, INVALID_LOCATION);
+        String expectedMessage = Location.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, event::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullLocation_throwsIllegalValueException() {
+        JsonAdaptedEvent event = new JsonAdaptedEvent(VALID_NAME, VALID_DATE, null);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Location.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, event::toModelType);
+    }
+
+
+}
