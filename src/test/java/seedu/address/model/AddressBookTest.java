4fe40d3faa636677e6a1a43570ab153dae2780bd
wr1159 <56021409+wr1159@users.noreply.github.com>
2023-10-11 09:26:06 -0400

    Add tests for AddressBook to include events cases

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -4,8 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_BOXING;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_LOCATION_BOXING;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalEvents.AURORA_BOREALIS;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
@@ -19,8 +22,10 @@ import org.junit.jupiter.api.Test;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import seedu.address.model.event.Event;
+import seedu.address.model.event.exceptions.DuplicateEventException;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.testutil.EventBuilder;
 import seedu.address.testutil.PersonBuilder;
 
 public class AddressBookTest {
@@ -50,7 +55,7 @@ public class AddressBookTest {
         Person editedAlice = new PersonBuilder(ALICE).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND)
                 .build();
         List<Person> newPersons = Arrays.asList(ALICE, editedAlice);
-        AddressBookStub newData = new AddressBookStub(newPersons);
+        AddressBookStub newData = new AddressBookStub(newPersons, Arrays.asList());
 
         assertThrows(DuplicatePersonException.class, () -> addressBook.resetData(newData));
     }
@@ -85,6 +90,41 @@ public class AddressBookTest {
     }
 
     @Test
+    public void resetData_withDuplicateEvents_throwsDuplicateEventException() {
+        // Two persons with the same identity fields
+        Event editedAurora = new EventBuilder(AURORA_BOREALIS).withDate(VALID_DATE_BOXING)
+                .withLocation(VALID_LOCATION_BOXING).build();
+        List<Event> newEvents = Arrays.asList(AURORA_BOREALIS, editedAurora);
+        AddressBookStub newData = new AddressBookStub(Arrays.asList(), newEvents);
+
+        assertThrows(DuplicateEventException.class, () -> addressBook.resetData(newData));
+    }
+
+    @Test
+    public void hasEvent_nullEvent_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> addressBook.hasEvent(null));
+    }
+
+    @Test
+    public void hasEvent_eventNotInAddressBook_returnsFalse() {
+        assertFalse(addressBook.hasEvent(AURORA_BOREALIS));
+    }
+
+    @Test
+    public void hasEvent_eventInAddressBook_returnsTrue() {
+        addressBook.addEvent(AURORA_BOREALIS);
+        assertTrue(addressBook.hasEvent(AURORA_BOREALIS));
+    }
+
+    @Test
+    public void hasEvent_eventWithSameIdentityFieldsInAddressBook_returnsTrue() {
+        addressBook.addEvent(AURORA_BOREALIS);
+        Event editedAurora = new EventBuilder(AURORA_BOREALIS).withDate(VALID_DATE_BOXING)
+                .withLocation(VALID_LOCATION_BOXING)
+                .build();
+        assertTrue(addressBook.hasEvent(editedAurora));
+    }
+    @Test
     public void toStringMethod() {
         String expected = AddressBook.class.getCanonicalName() + "{persons=" + addressBook.getPersonList()
                 + ", events=" + addressBook.getEventList() + "}";
@@ -98,8 +138,9 @@ public class AddressBookTest {
         private final ObservableList<Person> persons = FXCollections.observableArrayList();
         private final ObservableList<Event> events = FXCollections.observableArrayList();
 
-        AddressBookStub(Collection<Person> persons) {
+        AddressBookStub(Collection<Person> persons, Collection<Event> events) {
             this.persons.setAll(persons);
+            this.events.setAll(events);
         }
 
         @Override
