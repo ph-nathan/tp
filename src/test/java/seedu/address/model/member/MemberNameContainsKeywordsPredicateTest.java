6fc86beaaa82f9e390b2f581d23f0556688681a1
dylkaw <37286651+dylkaw@users.noreply.github.com>
2023-10-16 12:43:39 +0800

    Move MemberNameContainsKeywordsPredicateTest

--- src/test/java/seedu/address/model/member/MemberNameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/member/MemberNameContainsKeywordsPredicateTest.java
@@ -1,4 +1,4 @@
-package seedu.address.model.shared;
+package seedu.address.model.member;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
