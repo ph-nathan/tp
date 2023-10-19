21783165bb5be0f0a0ea04284f9364ca4c37a8c0
ph-nathan <px5099@gmail.com>
2023-10-19 16:56:05 +0800

    Fix checkstyle 3.0

--- src/test/java/seedu/address/storage/JsonAddressBookStorageTest.java
+++ src/test/java/seedu/address/storage/JsonAddressBookStorageTest.java
@@ -1,6 +1,8 @@
 package seedu.address.storage;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalAddressBook.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalMembers.ALICE;
